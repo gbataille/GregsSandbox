db2279b0792981d7a5c3e49a2be7250f07d3d4ca
Gregory Bataille <gregory.internet@gmail.com>
2011-10-11 20:22:03 +0200

    fibo finished

--- prolog/day2_fibonacci.pl
+++ prolog/day2_fibonacci.pl
@@ -1,4 +1,21 @@
-fib(0,1).
-fib(1,1).
-fib(N,F) :- N > 1, N1 is N - 1, N2 is N - 2,
-    fib(N1,F1), fib(N2,F2), F is F1 + F2.
+fib_naive(0,1) :- !.
+fib_naive(1,2) :- !.
+fib_naive(X,Y) :-
+    X > 1,
+    X1 is X - 1,
+    X2 is X - 2,
+    fib_naive(X1, Prev),
+    fib_naive(X2, PrePrev),
+    Y is Prev + PrePrev.
+    
+fib(0,1) :- !.
+fib(1,2) :- !.
+fib(X,Y) :-
+    X > 1,
+    fib_calc(1,2,X,Y).
+    
+fib_calc(_, F, 1, F) :- !.
+fib_calc(ST1, ST2, I, Result) :-
+    I1 is I - 1,
+    ST3 is ST1 + ST2,
+    fib_calc(ST2, ST3, I1, Result).
