6ede31de3d6103acdefaaff83fd24bb3f34f8a1a
Gregory Bataille <gbataille.dev@gmail.com>
2012-09-06 11:58:41 +0200

    Erlang day2 exercises

--- Erlang/7l7w/day2.erl
+++ Erlang/7l7w/day2.erl
@@ -1,7 +1,20 @@
 -module(day2).
--export([getFromKey/2]).
+-export([getFromKey/2, totalCartByProduct/1]).
+
+% Takes 
+%   - A list of tuples [{key, value}, ...]
+%   - A value to look for in the key list of the tuples list
+% Returns
+%   - The value associated to the passed key if it exists
+%   - false otherwise
 
 getFromKey([], _) -> false;
 getFromKey([{Key, Value}|_], Key) -> Value;
 getFromKey([{_, _}|Rest], Key) -> getFromKey(Rest,Key).
 
+% Takes
+%   - A shopping cart of the form [{item, quantity, unitPrice}, ...]
+% Returns
+%   - A list of items associated with their total price
+
+totalCartByProduct(X) -> [{Item, Quantity*Price} || {Item,Quantity,Price} <- X].
