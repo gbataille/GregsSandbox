5df9a2979c203072d440e2abb326d58a15404d05
Gregory Bataille <gbataille.dev@gmail.com>
2013-02-02 18:31:03 +0100

    finish chapter 5.

--- CommonLisp/LandOfLisp/textGame.lisp
+++ CommonLisp/LandOfLisp/textGame.lisp
@@ -1,3 +1,5 @@
+;;; START THE DATA DEFINITION
+
 (defparameter *nodes* '((living-room (you are in the living room.
                           a wizard is snoring loudly on the couch.))
                         (garden (you are in a beautiful garden.
@@ -5,19 +7,10 @@
                         (attic (you are in the attic.
                           there is a giant welding torch in the corner.))))
 
-(defun describe-location (location nodes)
-  (cadr (assoc location nodes)))
-
 (defparameter *edges* '((living-room (garden west door)
                                      (attic upstairs ladder))
                         (garden (living-room east door))
                         (attic (living-room downstairs ladder))))
-
-(defun describe-path (edge)
-  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
-
-(defun describe-paths (location edges)
-  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
 
 (defparameter *objects* '(whiskey bucket frog chain))
 
@@ -25,6 +18,21 @@
                                   (bucket living-room)
                                   (frog garden)
                                   (chain garden)))
+
+(defparameter *location* 'living-room)
+
+;;; STOP THE DATA DEFINITION
+;;; START THE LOGIC DEFINITION
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
 (defun objects-at (loc objs obj-loc)
   (labels ((at-loc-p (objs)
               (eq (cadr (assoc objs obj-loc)) loc)))
@@ -35,9 +43,29 @@
               `(you see a ,obj on the floor.)))
     (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
 
-(defparameter *location* 'living-room)
+;;; STOP LOGIC DEFINITION
+;;; START USER ACTIONS
 
 (defun look ()
   (append (describe-location *location* *nodes*)
           (describe-paths *location* *edges*)
           (describe-objects *location* *objects* *object-location*)))
+
+(defun walk (direction)
+  (let ((next (find direction
+                    (cdr (assoc *location* *edges*))
+                    :key #'cadr)))
+    (if next
+      (progn (setf *location* (car next))
+             (look))
+      '(you cannot go that way))))
+
+(defun pickup (object)
+  (cond ((member object
+                 (objects-at *location* *objects* *object-location*))
+         (push (list object 'body) *object-location*)
+         `(you are now carrying the ,object))
+        (t '(you cannot get that.))))
+
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-location*)))
