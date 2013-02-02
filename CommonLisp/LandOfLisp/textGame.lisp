c9d468f0de33bf0713d5c691db7276625142f7ab
Gregory Bataille <gbataille.dev@gmail.com>
2013-02-02 09:33:30 +0100

    Initial commit for the land of lisp. Text game. look functionality

---
+++ CommonLisp/LandOfLisp/textGame.lisp
@@ -0,0 +1,43 @@
+(defparameter *nodes* '((living-room (you are in the living room.
+                          a wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden.
+                          there is a well in front of you.))
+                        (attic (you are in the attic.
+                          there is a giant welding torch in the corner.))))
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(defparameter *edges* '((living-room (garden west door)
+                                     (attic upstairs ladder))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))))
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-location* '((whiskey living-room)
+                                  (bucket living-room)
+                                  (frog garden)
+                                  (chain garden)))
+(defun objects-at (loc objs obj-loc)
+  (labels ((at-loc-p (objs)
+              (eq (cadr (assoc objs obj-loc)) loc)))
+    (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-loc)
+  (labels ((describe-obj (obj)
+              `(you see a ,obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
+
+(defparameter *location* 'living-room)
+
+(defun look ()
+  (append (describe-location *location* *nodes*)
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-location*)))
