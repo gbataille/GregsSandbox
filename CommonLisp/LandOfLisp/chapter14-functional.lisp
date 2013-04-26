29e5a250789ea13c3bd9832c57b78b0d9342ca7a
Gregory Bataille <gbataille.dev@gmail.com>
2013-04-26 22:42:32 +0200

    Next chapters

---
+++ CommonLisp/LandOfLisp/chapter14-functional.lisp
@@ -0,0 +1,11 @@
+;the clean part, functional part
+(defun add-widget (database widget)
+  (cons widget database))
+
+;the dirty, nonfunctional part
+(defparameter *database* nil)
+
+(defun main-loop ()
+  (loop (princ "Please enter the name of a new widget")
+        (setf *database* (add-widget *database* (read)))
+        (format t "The database contains the following: ~a~%" *database*)))
