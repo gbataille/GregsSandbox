df6cda1073d82568f53901868ed9c2164d03d0ac
Gregory Bataille <gbataille.dev@gmail.com>
2013-05-26 14:35:56 +0200

    solution to QualifA Afriqua 2010

--- go/CodeJam/Africa2010/QualifA.go
+++ go/CodeJam/Africa2010/QualifA.go
@@ -3,6 +3,7 @@ import (
   "fmt";
   "bufio";
   "strconv";
+  "strings";
   // "io";
   "os"
 )
@@ -10,18 +11,83 @@ import (
 func main() {
   args := os.Args
   //Display help
-  if len(args) != 2 {
+  if len(args) != 3 {
     manual()
     return
   }
 
   filename := args[1]
+  outputFile := args[2]
   headerLines := 1
   caseSize := 3
 
   _, cases := readInputFile(filename, headerLines, caseSize)
+  solutions := make([][]int, len(cases), len(cases))
 
-  fmt.Println(cases)
+  i := 0
+  for i < len(cases) {
+    budget, _ := strconv.Atoi(cases[i][0])
+    numObjects, _ := strconv.Atoi(cases[i][1])
+    articles := strings.Split(cases[i][2], " ")
+    prices := make([]int, len(articles), len(articles))
+    k := 0
+    for k < len(articles) {
+      prices[k], _ = strconv.Atoi(articles[k])
+      k += 1
+    }
+    j := 0
+
+    for j < numObjects {
+      curObj := prices[j]
+      if curObj < budget {
+        subPrices := prices[j+1:]
+        l := 0
+        for l < len(subPrices) {
+          if curObj + subPrices[l] == budget {
+            solutions[i] = []int{j+1, l+j+2}
+          }
+          l += 1
+        }
+      }
+
+      j += 1
+    }
+
+    i += 1
+  }
+
+  outputResults(solutions, outputFile)
+}
+
+func outputResults(results [][]int, outputFile string) {
+  fo, err := os.Create(outputFile)
+  if err != nil {
+    panic(err)
+  }
+  defer func() {
+    err := fo.Close()
+    if err != nil {
+      panic(err)
+    }
+  }()
+
+  w := bufio.NewWriter(fo)
+  i := 0
+  for i < len(results) {
+    a := results[i][0]
+    b := results[i][1]
+    _, err = w.WriteString(
+      fmt.Sprintf("Case #%d: %d %d\n", i+1, a, b))
+    if err != nil {
+      panic(err)
+    }
+
+    i += 1
+  }
+
+  if err = w.Flush(); err != nil {
+    panic(err)
+  }
 }
 
 func readInputFile(filename string,  headerLines int, 
@@ -72,5 +138,5 @@ func readInputFile(filename string,  headerLines int,
 }
 
 func manual() {
-  fmt.Println("Just pass the input filename as an argument")
+  fmt.Println("Just pass the input filename as the first argument and the output file as the second")
 }
